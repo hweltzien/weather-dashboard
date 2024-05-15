4c80b4286d17ce8d28c2d786c3746f13659f61c0
hweltzien <hmweltzien@gmail.com>
2024-05-15 13:42:24 -0700

    new function fetchWeather Data

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,49 @@ event.preventDefault()
 
 weather()
 })
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// function displayWeather(fetchWeatherData) {
+//     const cityName = 
+//     const Date =
+//     const iconCode =
+//     const temperature =
+//     const humidity = weatherData
+//     const windSpeed = weatherData.wind.speed;
+// }
 
 
+
+
+// function displayForecat(forecastData) {
+//     forecastData.forEach
+// }
+
+
+
+
+function fetchWeatherData () {
+    let lat = weather(data.list[1]);
+    let lon = weather(data.list[3]);
+    let weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c3e368d857c35c5a4b82b3d376f7b5f0`;
+    
+    fetch(weatherUrl)
+.then(function(response){
+   if (!response.ok) {
+    throw new Error('Bad network response');
+   }
+   console.log(response)
+   return response.json();
+})
+.then(function (data){
+    displayWeather(data.list[0]);
+    displayForecast(data.list.slice[1,6]);
+    console.log(data)
+})
+.catch(function(error){
+
+})
+}
 //create autocomplete form
+
+//access html and dynamically create html elements
+
