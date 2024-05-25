234f705eb11d3f0cc3b86b94252f82fafc4057aa
hweltzien <hmweltzien@gmail.com>
2024-05-25 15:20:40 -0700

    JS complete

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
 # weather-dashboard
+
+## Description
+"MyBrewFinder" is a web app for travelers, passionate about local beverages. Using the Wikipedia API, it provides information about the destination, including population, a general description, and location. Leveraging the Yelp Fusion API, the app identifies and reviews coffee shops (or breweries) in the user provided city. This dual functionality helps travelers plan visits to local coffee shops and breweries, ensuring a rich tasting experience.
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [How-To-Guide](#how-to-guide)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+To clone the application, click the green button labeled, "code." Copy the URL for the repository, using HTTPS, SSH, or GitHub CLI. Open Git Bash. Change the working directory to the desired location for the cloned directory. Type "git clone" and paste the copied URL. Press "enter" to create your local clone.
+
+## Usage
+
+Beginning on "index.html," the user will see the "My Brew Finder" logo in the header and a form to collect user input. After completing the form and upon clicking the "FIND MY BREW!" button, the user is directed to "info.html." The user will see the heading, "THE PERFECT BREW, FOUND FOR YOU!" and is presented with search results from Wikipedia and Yelp. There is a modal which provides information about the creators. The user can click the "go back" button to return to the home page.
+
+
+<img src= "./assets/images/Screenshot1.png" alt="screenshot">
+<img src= "./assets/images/Screenshot2.png" alt="screenshot">
+
+
+## How-To-Guide
+<ul>    
+    <li>GIVEN a weather dashboard with form inputs</li>
+    <li>WHEN I search for a city</li>
+    <li>THEN I am presented with current and future conditions for that city and that city is added to the search history</li>
+    <li>WHEN I view current weather conditions for that city</li>
+    <li>THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed</li>
+    <li>WHEN I view future weather conditions for that city</li>
+    <li>THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity</li>
+    <li>WHEN I click on a city in the search history</li>
+    <li>THEN I am again presented with current and future conditions for that city</li>
+</ul>
+
+## Credits
+<ul>
+    <li>Tutoring session with Jaytee Padilla</li>
+    <li>Tutoring session with Michell Brito</li>
+    <li>Tutoring session with Juan Delgado</li>
+    <li>Tutoring session with Charles Puente-Matos</li>
+    <li>Tutoring session with Eurel Rambaran</li>
+    <li>Tutoring session with Ntombifuthi Motaung</li>
+    <li>Office hours with Anthony Barragan</li>
+    <li>Office hours with Erik Hirsch</li>
+    <li><a href=" https://www.youtube.com/watch?v=6trGQWzg2AI&t=4175s">Demo</a></li>
+   
+    
+</ul>
+
+## License
+ 
+Please refer to the LICENSE in the repo.
+
+# [link to completed project](______________)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,95 +1,202 @@
 
 const searchFormEl = document.querySelector('#search-form');
 const cityNameEl = document.getElementById("cityName")
-function handleSearchFormSubmit(event) {
-    event.preventDefault();
-    const searchInputVal = document.querySelector('#search-input').value;
-        if(!searchInputVal) {
-            console.error('Please enter a city name.');
-            return;
-        }
-    const queryString = `./search-results.html?q=${searchInputVal}`;
-    location.assign(queryString);
+const windSpeedEl = document.getElementById("wind-speed")
+const humidityEl = document.getElementById("humidity")
+const temperatureEl = document.getElementById("temp")
+const iconCodeEl = document.getElementById("weather-icon")
+const dateEl = document.getElementById("date")
+const timeEl = document.getElementById("time")
+const btn = document.getElementById("searchBtn")
+const currentWeatherItemsEl = document.getElementById("currentWeatherItems")
+const addCityEl = document.getElementById("addCity")
+const currentTempEl = document.getElementById("temp")
+const pastCitiesEl = document.getElementById("pastCities")
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const weatherForecastEl = document.getElementById("weatherForecast")
+const savedCities = JSON.parse(localStorage.getItem('cities')) || [];
+
+
+
+const renderDaysOfTheWeek = () => {
+
+    for (let i = 0; i<5; i++) {
+        const currentDayDiv = document.getElementById(`day-${i}`)
+        const a = dayjs()
+        const b = a.add(i, 'day')
+        console.log(b)
+        let day = String(b.$d).split(" ")
+        day = day.slice(0, 3).join(", ")
+        
+        currentDayDiv.innerText = day
+        
+    }
 }
 
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
-function displayWeather(fetchWeatherData) {
-    console.log(fetchWeatherData)
-    const cityName = fetchWeatherData.name;
-    console.log(cityName)
-    // const Date = new Date(fetchWeatherData.dt * 1000);
-    // const iconCode = fetchWeatherData.weather[0].icon;
-    const temperature = fetchWeatherData.main.temp;
-    const humidity = weatherData.main.humidity;
-    const windSpeed = weatherData.wind.speed;
-console.log(windSpeed)
-   cityNameEl.textContent = cityName;
-   console.log(cityNameEl)
-    // document.getElementById("date").textContent = date.toDateString();
-    // document.getElementById("weather-icon").src = `https://openweathermap.org/img/wn/${iconCode}.png`;
-    // document.getElementById("temperature").textContent = temperature;
-    // document.getElementById("humidity").textContent = humidity;
-    // document.getElementById("wind-speed").textContent = windSpeed;
+
+setInterval(() => {
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
+    const minutes = time.getMinutes();
+    const ampm = hour >= 12 ? 'PM' : 'AM'
+
+   
+    dateEl.innerHTML = days[day] + ', ' + months[month] + ' ' + date
+    timeEl.innerHTML = new Date().toLocaleTimeString();
+}, 1000);
+
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+   
+    const searchInputVal = document.querySelector('#search-input').value;
+    if (!searchInputVal) {
+        console.error('Please enter a city name.');
+        return;
+    }
+    savedCities.push(searchInputVal)
+    localStorage.setItem('cities', JSON.stringify(savedCities))
+    renderSavedCities()
+    
+    
 }
-function weather () {
-    let cityName = document.getElementById("search-input").value
+function renderSavedCities() {
+    const historyData = localStorage.getItem('cities')
+    const cities =  JSON.parse(historyData)
+    pastCitiesEl.innerHTML= " ";
+    console.log("savedCities", typeof cities)
+    for (let i = 0; i < cities.length; i++) {
+        //create button
+        const pastCityBtn = document.createElement("button")
+        
+        //add text
+        pastCityBtn.textContent = cities[i]
+        
+        //add event listener
+        pastCityBtn.addEventListener('click', handleCityBtnSubmit);
+        
+        
+        //append to div
+        
+        pastCitiesEl.appendChild(pastCityBtn)
+    
+        
+    }
+}
+
+
+
+
+
+//attach the latitude and longitude to the text content of the search history button 
+function handleCityBtnSubmit(event) {
+    event.preventDefault();
+    console.log(this.textContent)
+    const savedCityBtn = this.textContent
+    
+   
+    GetLatLon(savedCityBtn)
+    
+    
+}
+
+btn.addEventListener('click', handleSearchFormSubmit);
+
+function displayWeather(fetchWeatherData) {
+
+    console.log(fetchWeatherData)
+    const cityName = fetchWeatherData.city.name;
+    console.log(cityName)
+
+    const currentWeather = fetchWeatherData.list[0]
+    console.log(currentWeather)
+    const iconCode = currentWeather.weather[0].icon;
+    const temperature = currentWeather.main.temp;
+    const humidity = currentWeather.main.humidity;
+    const windSpeed = currentWeather.wind.speed;
+
+    cityNameEl.textContent = cityName;
+    windSpeedEl.textContent = windSpeed;
+    humidityEl.textContent = humidity;
+    temperatureEl.innerHTML = `${temperature} &#176; F`;
+    
+    iconCodeEl.setAttribute('src', `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@4x.png`);
+
+    
+    let row = document.querySelectorAll('.weatherForecastItem');
+    for (let i = 0; i < row.length; i++) {
+        row[i].innerHTML = 
+       
+        
+       `<img src="https://openweathermap.org/img/wn/${fetchWeatherData.list[1].weather[0].icon}@2x.png" alt="weather icon" class="weather-icon"> 
+        <p>Temperature</p>
+        <p class="temp">${fetchWeatherData.list[i].main.temp}&#176; F</p> <p>Humidity</p>
+        <p id="humidity">${fetchWeatherData.list[i].main.humidity}</p>
+        <p>Wind Speed</p>
+        <p id="wind-speed">${fetchWeatherData.list[i].wind.speed}</p>`;
+
+    }
+    renderDaysOfTheWeek()
+
+
+}
+function GetLatLon(cityName) {
     let geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=c3e368d857c35c5a4b82b3d376f7b5f0`;
     console.log(cityName)
     fetch(geoUrl)
-.then(function(response){
-    console.log(response)
-    return response.json();
-})
-.then(function (data){
-    console.log(data)
-    fetchWeatherData(data[0].lat, data[0].lon)
-})
-//make another API call, 1. grab lat and lon data out of response from first API call, 2. feed it into the second API call in the same way we did the first call
-}
-document.getElementById("searchBtn").addEventListener("click", function(event){
-event.preventDefault()
-
-weather()
-})
-
-
-
-
-
-
-// function displayForecat(forecastData) {
-//     forecastData.forEach(function(dayData){
-
-// })
-// }
-
-
-
-
-function fetchWeatherData (lat,lon) {
-   
-    let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c3e368d857c35c5a4b82b3d376f7b5f0`;
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data)
+        fetchWeatherData(data[0].lat, data[0].lon)
+    })
     
-    fetch(weatherUrl)
-.then(function(response){
-   if (!response.ok) {
-    throw new Error('Bad network response');
-   }
-
-   console.log(response)
-   return response.json();
-})
-.then(function (data){
-    displayWeather(data.city);
-    // displayForecast(data.list.slice[1,6]);
-    console.log(data)
-})
-.catch(function(error){
-
-})
 }
-//create autocomplete form
+document.getElementById("searchBtn").addEventListener("click", function (event) {
+    event.preventDefault()
+    let cityName = document.getElementById("search-input").value
+        
+    
+    
+        GetLatLon(cityName)
+})
 
-//access html and dynamically create html elements
 
-// icon code https://openweathermap.org/img/wn/10n@2x.png
+
+
+function fetchWeatherData(lat, lon) {
+
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=c3e368d857c35c5a4b82b3d376f7b5f0`;
+
+    fetch(weatherUrl)
+        .then(function (response) {
+            if (!response.ok) {
+                throw new Error('Bad network response');
+            }
+
+            console.log(response)
+            return response.json();
+        })
+        .then(function (data) {
+            displayWeather(data);
+            console.log(data)
+
+        })
+        .catch(function (error) {
+
+        })
+}
+
+
+
+renderDaysOfTheWeek()
+renderSavedCities()
+
+//how is content in cards getting put in there....it's being overwritten...get rid of what's overwriting 
